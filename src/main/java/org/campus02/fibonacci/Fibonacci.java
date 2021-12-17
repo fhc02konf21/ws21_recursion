c4c1e4c94bf4ad8a33545ebb19d1bd87c1736255
dkandlhofer <dkandlhofer@know-center.at>
2021-12-17 20:44:34 +0100

    fibonacci

--- src/main/java/org/campus02/fibonacci/Fibonacci.java
+++ src/main/java/org/campus02/fibonacci/Fibonacci.java
@@ -10,6 +10,9 @@ public class Fibonacci {
      * @return  Wert der n-ten Stelle
      */
     public int fib(int n) {
-        return 0;
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n - 1) + fib(n - 2);
     }
 }
